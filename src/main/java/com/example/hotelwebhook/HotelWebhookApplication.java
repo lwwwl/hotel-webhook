5c39911eac355f0896ca9a0bc96fd7ff0d7c5701
lao-desktop <lwggcn@gmail.com>
2025-07-28 21:01:17 +0800

    init

---
+++ src/main/java/com/example/hotelwebhook/HotelWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelWebhookApplication.class, args);
+    }
+
+}
